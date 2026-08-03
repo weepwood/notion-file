dc2df654c247d46c214f99d6aa1a995515ba1497
weepwood <weepwood@outlook.com>
2026-08-04 01:18:22 +0800

    feat: 默认启用文件管理中心

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import FileManagerApp from "./FileManagerApp";
 import "./styles.css";
 import "./advanced.css";
+import "./file-manager.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <FileManagerApp />
   </React.StrictMode>,
 );
