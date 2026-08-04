393457e7b243cb32c96fc652d11f8dd62e62e8b7
weepwood <weepwood@outlook.com>
2026-08-04 13:11:39 +0800

    fix: 自动同步界面显示版本

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import FileManagerApp from "./FileManagerApp";
+import { installVersionLabelSync } from "./version-label";
 import "./styles.css";
 import "./advanced.css";
 import "./file-manager.css";
+
+const disposeVersionLabelSync = installVersionLabelSync();
+if (import.meta.hot) {
+  import.meta.hot.dispose(disposeVersionLabelSync);
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
